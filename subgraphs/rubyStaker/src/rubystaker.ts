47929cb85853db69e0057aa5857ba5533ce1161b
AnichoKraus <anichokraus3123@gmail.com>
2022-09-14 02:28:45 -0400

    add RewardPaid & ExpiredLocksWithdrawal Events

--- subgraphs/rubyStaker/src/rubystaker.ts
+++ subgraphs/rubyStaker/src/rubystaker.ts
@@ -1,9 +1,9 @@
-import { Staked, Withdrawal } from '../generated/Staker/Staker'
+import { Staked, Withdrawal, ExpiredLocksWithdrawal, RewardPaid } from '../generated/Staker/Staker'
 import { Stake } from '../generated/schema'
 
 import { log } from '@graphprotocol/graph-ts'
   
-import { BIG_DECIMAL_1E18 } from 'const'
+import { BIG_DECIMAL_1E18, ADDRESS_ZERO } from 'const'
 
 export function onStake(event: Staked): void {  
   log.info('{} has staked {} ruby', [
@@ -15,7 +15,8 @@ export function onStake(event: Staked): void {
 
   staked.address = event.params.user
   staked.amount = event.params.amount.toBigDecimal().div(BIG_DECIMAL_1E18)
-  staked.stake = true
+  staked.type = 'Staked'
+  staked.rewardToken = ADDRESS_ZERO
   staked.timestamp = event.block.timestamp
 
   staked.save()
@@ -31,7 +32,42 @@ export function onWithdraw(event: Withdrawal): void {
 
   staked.address = event.params.user
   staked.amount = event.params.amount.toBigDecimal().div(BIG_DECIMAL_1E18)
-  staked.stake = false
+  staked.type = 'Withdrawal'
+  staked.rewardToken = ADDRESS_ZERO
+  staked.timestamp = event.block.timestamp
+
+  staked.save()
+}
+
+export function onExpiredLocksWithdrawal(event: ExpiredLocksWithdrawal): void {  
+  log.info('{} has withdrawed {} ruby', [
+    event.params.user.toHex(),
+    event.params.amount.toString()
+  ])
+
+  let staked = new Stake(event.transaction.hash.toHex())
+
+  staked.address = event.params.user
+  staked.amount = event.params.amount.toBigDecimal().div(BIG_DECIMAL_1E18)
+  staked.type = 'ExpiredLocksWithdrawal'
+  staked.rewardToken = ADDRESS_ZERO
+  staked.timestamp = event.block.timestamp
+
+  staked.save()
+}
+
+export function onRewardPaid(event: RewardPaid): void {  
+  log.info('{} has withdrawed {} ruby', [
+    event.params.user.toHex(),
+    event.params.reward.toString()
+  ])
+
+  let staked = new Stake(event.transaction.hash.toHex())
+
+  staked.address = event.params.user
+  staked.amount = event.params.reward.toBigDecimal()
+  staked.type = 'RewardPaid'
+  staked.rewardToken = event.params.rewardToken
   staked.timestamp = event.block.timestamp
 
   staked.save()
